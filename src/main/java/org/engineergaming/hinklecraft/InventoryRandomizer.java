13c369004e35445839c01dfc2c841d8e1fd87ba9
SmonkChan <elijahjrichards@gmail.comn>
2025-07-28 10:58:27 -0500

    Fixed by adding bukkit scheduler

--- src/main/java/org/engineergaming/hinklecraft/InventoryRandomizer.java
+++ src/main/java/org/engineergaming/hinklecraft/InventoryRandomizer.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.Material;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
@@ -12,12 +13,19 @@ public class InventoryRandomizer implements Listener{
     private static final int changeChance = 10;
     private final Random random = new Random();
 
+    Main plugin = Main.getPlugin(Main.class);
+
     @EventHandler
     public void onMoveEvent(PlayerMoveEvent event) {
         if(random.nextInt(changeChance)==1){
             Material newMat = Material.values()[random.nextInt(Material.values().length)];
             if (newMat.isItem()) {
-                event.getPlayer().getInventory().setItem(random.nextInt(27)+9, new ItemStack(newMat, newMat.getMaxStackSize()));
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        event.getPlayer().getInventory().setItem(random.nextInt(27)+9, new ItemStack(newMat, newMat.getMaxStackSize()));
+                    };
+                }.runTaskLater(plugin, 1L);
             }
         }
     }

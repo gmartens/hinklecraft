f4d9e254b23583bf5ce553e656d22b64b711d7b3
SmonkChan <elijahjrichards@gmail.comn>
2025-07-31 14:33:51 -0500

    Changed Function, works with fortune and not with silk touch

--- src/main/java/org/engineergaming/hinklecraft/CoalListener.java
+++ src/main/java/org/engineergaming/hinklecraft/CoalListener.java
@@ -4,7 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.inventory.ItemStack;
 
@@ -12,17 +12,28 @@ import java.util.Random;
 
 public class CoalListener implements Listener {
     private final Random random = new Random();
+    int totalItems = 0;
 
     @EventHandler
-    public void onCoalBreak(BlockBreakEvent event) {
-        if (event.isDropItems() && ((event.getBlock().getType() == Material.COAL_ORE) || (event.getBlock().getType() == Material.DEEPSLATE_COAL_ORE))) {
+    public void onCoalBreak(BlockDropItemEvent event) {
+        if ((event.getBlockState().getBlock().getType() == Material.COAL_ORE) || (event.getBlockState().getBlock().getType() == Material.DEEPSLATE_COAL_ORE)) {
+            boolean coalFound = false;
+            for (int i = 0; i < event.getItems().size(); i++) {
+                if (event.getItems().get(i).getItemStack().getType() == Material.COAL) {
+                    coalFound = true;
+                    totalItems += event.getItems().get(i).getItemStack().getAmount();
+                }
+            }
+            if (!coalFound) {return;}
             World world = event.getBlock().getWorld();
             // Spawn between 4 and 6 stacks of extra items
-            int extra = 258+random.nextInt(129); 
+            int extra = 32+random.nextInt(16); 
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    world.dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.COAL, extra));
+                    for (int i = 0; i < extra; i++) {
+                        world.dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.COAL, totalItems*8));
+                    }
                 }
             }.runTaskLater(Main.getPlugin(Main.class), 1L);
         }
